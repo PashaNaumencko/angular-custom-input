69a79273bb364fadf7ae4c0d383f771af0bdab7b
Pavlo Naumenko <pashanaumencko@gmail.com>
2020-09-28 20:27:59 +0300

    Update form, add custom input, add delay on submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,33 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent {
   form: FormGroup;
+  loading: boolean = false;
 
   constructor(fb: FormBuilder) {
     this.form = fb.group({
       name: ['', [Validators.required]],
       customField: ['', [Validators.required]]
+    }, {
+      // Delay updating the form validity for performance reasons
+      updateOn: 'blur'
     });
+  }
+
+  get nameFormControl() {
+    return this.form.controls.name;
+  }
+
+  get customInputFormControl() {
+    return this.form.controls.customField;
+  }
+
+  onSubmit() {
+    if(this.form.valid) {
+      this.loading = true;
+      timer(500).subscribe(() => {
+        console.log(this.form.value);
+        this.loading= false;
+      });
+    }
   }
 }
--- src/app/custom-input/custom-input.component.ts
+++ src/app/custom-input/custom-input.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-custom-input',
   templateUrl: './custom-input.component.html',
-  styleUrls: ['./custom-input.component.scss']
+  styleUrls: ['./custom-input.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => CustomInputComponent),
+    multi: true
+  }]
 })
-export class CustomInputComponent implements OnInit {
+export class CustomInputComponent implements ControlValueAccessor {
+  @Input() label: string;
+  @Input() placeholder = '';
+  @Input() hasError: boolean;
 
-  constructor() { }
+  // The internal data model
+  private innerValue = '';
 
-  ngOnInit(): void {
+  // Change inner value on input
+  onChange(value: string): void {
+    this.writeValue(value);
+    this.onTouchedCallback();
   }
 
+  // Set touched on blur
+  onBlur(): void {
+    this.onTouchedCallback();
+  }
+
+  // Placeholders for the callbacks which are later provided
+  // by the Control Value Accessor
+  private onTouchedCallback: () => void;
+  private onChangeCallback: (_: string) => void;
+
+  // From ControlValueAccessor interface
+  writeValue(value: string): void {
+    // Check if inner value is changed for performance reasons
+    if (value !== this.innerValue) {
+      this.innerValue = value;
+      this.onChangeCallback(value);
+    }
+  }
+
+  // From ControlValueAccessor interface
+  registerOnChange(fn: any): void {
+    this.onChangeCallback = fn;
+  }
+
+  // From ControlValueAccessor interface
+  registerOnTouched(fn: any): void {
+    this.onTouchedCallback = fn;
+  }
 }
